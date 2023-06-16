00bbe50bfd7ab9a13e0c7c418617ffe0d5a0af04
Krisna31 <krisnaaaputraaa@gmail.com>
2023-06-16 21:51:11 +0700

    add while

--- script-jelvin_krisna_putra.sh
+++ script-jelvin_krisna_putra.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Print memory size in megabytes
-echo "Memory Size:"
-free -m
-sleep 3s
+# while loop so the script run forever until stopped with ctrl+c
+while true
+do
+  # Print memory size in megabytes
+  echo "Memory Size:"
+  free -m
+  sleep 3s
 
-# Print disk usage in gigabytes
-echo " "
-echo "Disk Usage:"
-df -h --output=source,size --exclude-type=tmpfs
-sleep 3s
+  # Print disk usage in gigabytes
+  echo " "
+  echo "Disk Usage:"
+  df -h --output=source,size --exclude-type=tmpfs
+  sleep 3s
 
-# Print filesystem usage and Use%
-echo " "
-echo "Filesystem Usage:"
-df -h --output=source,pcent --exclude-type=tmpfs
-sleep 1m
-
+  # Print filesystem usage and Use%
+  echo " "
+  echo "Filesystem Usage:"
+  df -h --output=source,pcent --exclude-type=tmpfs
+  sleep 1m
+done
